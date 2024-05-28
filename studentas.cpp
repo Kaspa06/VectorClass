4c41ff5e3998e7cf2563bb450381960718057c82
Kasparas <159523672+Kaspa06@users.noreply.github.com>
2024-05-28 21:22:09 +0300

    Update studentas.cpp

--- studentas.cpp
+++ studentas.cpp
@@ -1,7 +1,7 @@
 #include "studentas.h"
+#include "vector.h" // Include your custom vector header
 #include <algorithm>
 #include <cstdlib> 
-#include <fstream>
 #include <iostream>
 
 // Constructor
@@ -19,14 +19,14 @@ Studentas::~Studentas() {
     egzaminas = 0;
 }
 
-//Copying constructor
+// Copy constructor
 Studentas::Studentas(const Studentas &copy)
 : vardas(copy.vardas), pavarde(copy.pavarde), nd_rezultatai(copy.nd_rezultatai), egzaminas(copy.egzaminas) {}
 
-//Copy assignment
+// Copy assignment operator
 Studentas& Studentas::operator=(const Studentas& copy) 
 {
-    if(this !=&copy)
+    if(this != &copy)
     {
         vardas = copy.vardas;
         pavarde = copy.pavarde;
@@ -36,47 +36,43 @@ Studentas& Studentas::operator=(const Studentas& copy)
     return *this;
 }
 
-// Move Constructor
-Studentas::Studentas(Studentas&& copy) noexcept{
-    vardas = std::move(copy.vardas);
-    pavarde = std::move(copy.pavarde);
-    nd_rezultatai = std::move(copy.nd_rezultatai);
-    egzaminas = copy.egzaminas;
-}
+// Move constructor
+Studentas::Studentas(Studentas&& copy) noexcept
+: vardas(std::move(copy.vardas)), pavarde(std::move(copy.pavarde)), nd_rezultatai(std::move(copy.nd_rezultatai)), egzaminas(copy.egzaminas) {}
 
- // Move assignment operator
-    Studentas& Studentas::operator=(Studentas&& copy) noexcept {
-        if (this!= &copy) {
-            // Swap the members of the current object with the members of the other object
-            std::swap(vardas, copy.vardas);
-            std::swap(pavarde, copy.pavarde);
-            std::swap(nd_rezultatai, copy.nd_rezultatai);
-            std::swap(egzaminas, copy.egzaminas);
-        }
-        return *this;
+// Move assignment operator
+Studentas& Studentas::operator=(Studentas&& copy) noexcept {
+    if (this != &copy) {
+        std::swap(vardas, copy.vardas);
+        std::swap(pavarde, copy.pavarde);
+        std::swap(nd_rezultatai, copy.nd_rezultatai);
+        std::swap(egzaminas, copy.egzaminas);
     }
+    return *this;
+}
 
 // Public member functions
 void Studentas::setVardas(std::string vardas) {
-    this-> vardas = vardas;
+    this->vardas = vardas;
 }
+
 std::string Studentas::getVardas() const {
     return vardas;
 }
 
 void Studentas::setPavarde(std::string pavarde) {
-    this -> pavarde = pavarde;
+    this->pavarde = pavarde;
 }
 
 std::string Studentas::getPavarde() const {
     return pavarde;
 }
 
-void Studentas::setNamuDarbai(const std::vector<int>& nd) {
+void Studentas::setNamuDarbai(const vector<int>& nd) {
     nd_rezultatai = nd;
 }
 
-std::vector<int> Studentas::getNamuDarbai() const {
+vector<int> Studentas::getNamuDarbai() const {
     return nd_rezultatai;
 }
 
@@ -103,15 +99,14 @@ double Studentas::calcVidurkis() const {
 
 double Studentas::calcMediana() const {
     if (nd_rezultatai.empty()) return 0.0;
-    std::vector<int> tempNamuDarbai = nd_rezultatai;
+    vector<int> tempNamuDarbai = nd_rezultatai;
     std::sort(tempNamuDarbai.begin(), tempNamuDarbai.end());
     int dydis = tempNamuDarbai.size();
     return (dydis % 2 == 0) ? (tempNamuDarbai[dydis / 2 - 1] + tempNamuDarbai[dydis / 2]) / 2.0 : tempNamuDarbai[dydis / 2];
 }
 
 double Studentas::calcGalutinis(bool useVidurkis) const {
-    double galutinis = useVidurkis ? (0.4 * calcVidurkis() + 0.6 * egzaminas) : (0.4 * calcMediana() + 0.6 * egzaminas);
-    return galutinis;
+    return useVidurkis ? (0.4 * calcVidurkis() + 0.6 * egzaminas) : (0.4 * calcMediana() + 0.6 * egzaminas);
 }
 
 void Studentas::randomND() {
@@ -125,26 +120,25 @@ void Studentas::randomND() {
 void Studentas::randomStudentai() {
     const char* vardai[] = {"Jonas", "Petras", "Antanas", "Juozas", "Kazimieras"};
     const char* pavardes[] = {"Jonaitis", "Petraitis", "Antanaitis", "Juozaitis", "Kazimieraitis"};
-    int vardasIndex = rand() % 6;
-    int pavardeIndex = rand() % 6;
+    int vardasIndex = rand() % 5;
+    int pavardeIndex = rand() % 5;
     vardas = vardai[vardasIndex];
     pavarde = pavardes[pavardeIndex];
     randomND();
 }
 
-// Output Operator (Serialization)
+// Output operator (Serialization)
 std::ostream& operator<<(std::ostream& output, const Studentas &student) {
     output << student.vardas << " " << student.pavarde << " " << student.egzaminas << " ";
     for (int pazymys : student.nd_rezultatai) {
-        output << std::to_string(pazymys) << " "; // Pries printinant pakeist int'a i string'a
+        output << pazymys << " ";
     }
     return output;
 }
 
-// Input Operator (Deserialization)
+// Input operator (Deserialization)
 std::istream& operator>>(std::istream& input, Studentas &student) {
-    input >> student.vardas >> student.pavarde;
-    input >> student.egzaminas;
+    input >> student.vardas >> student.pavarde >> student.egzaminas;
     student.nd_rezultatai.clear();
     int pazymys;
     while (input >> pazymys) {
