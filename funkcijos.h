582116798fbc0b55b792dd3d50577cc12d91aa58
Kasparas <159523672+Kaspa06@users.noreply.github.com>
2024-05-28 21:21:35 +0300

    Update funkcijos.h

--- funkcijos.h
+++ funkcijos.h
@@ -2,14 +2,14 @@
 #define FUNKCIJOS_H
 
 #include "studentas.h"
-#include <vector>
+#include "vector.h"
 #include <string>
 
-void spausdintiGalutiniusBalus(const std::vector<Studentas>& studentai, const std::string& isvedimoFailoVardas = "", int rusiavimoTipas = 1);
-void manualInput(std::vector<Studentas>& studentai);
-void generateGradesOnly(std::vector<Studentas>& studentai);
-void readFileDataFromFile(std::vector<Studentas>& studentai, const std::string& failoVardas);
-void generateStudentFiles(const std::vector<int>& sizes);
-void rusiuotiStudentus(const std::vector<int>& sizes);
+void spausdintiGalutiniusBalus(const vector<Studentas>& studentai, const std::string& isvedimoFailoVardas = "", int rusiavimoTipas = 1);
+void manualInput(vector<Studentas>& studentai);
+void generateGradesOnly(vector<Studentas>& studentai);
+void readFileDataFromFile(vector<Studentas>& studentai, const std::string& failoVardas);
+void generateStudentFiles(const vector<int>& sizes);
+void rusiuotiStudentus(const vector<int>& sizes);
 
 #endif
